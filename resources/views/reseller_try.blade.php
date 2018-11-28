921cdf6d9d050512fc524fc7ffab20da56ca61e3
ajinkyameister <42531947+ajinkyameister@users.noreply.github.com>
2018-11-29 01:32:01 +0530

    Create reseller_try.blade.php

---
+++ resources/views/reseller_try.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('mainbody')
+
+	<div id="app" class="Box">
+		<menu>
+			example-component Vue Component is Working!! Congratulations!!!
+		</menu>
+
+	</div>
+
+@endsection	
